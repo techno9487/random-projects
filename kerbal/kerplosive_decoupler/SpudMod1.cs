d482095c427a036f24c2a31bc13581a3bfb6583b
techno9487 <tobythedude99@gmail.com>
2013-12-16 07:34:31 +0000

    First Release

---
+++ kerbal/kerplosive_decoupler/SpudMod1.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+namespace SpudMod1
+{
+    public class SpudMod1 : PartModule
+    {
+        private bool displaySettings = false;
+
+        [KSPEvent(guiActive=true,guiName="Destroy",active=true)]
+        public void Destroy()
+        {
+            RenderingManager.RemoveFromPostDrawQueue(0, OnDraw);
+            this.part.explode();
+        }
+
+        [KSPEvent(guiActive=true,guiName="Destroy Settings",active=true)]
+        public void destroySetting()
+        {
+            if (displaySettings == true)
+            {
+                RenderingManager.AddToPostDrawQueue(0, OnDraw);
+            }
+            else if (displaySettings == false)
+            {
+                RenderingManager.RemoveFromPostDrawQueue(0, OnDraw);
+            }
+            displaySettings = !displaySettings;
+        }
+
+        public void OnDraw()
+        {
+            GUI.skin = HighLogic.Skin;
+            if(GUI.Button(new Rect(100,100,100,100),"Click Me!")){
+            }
+        }
+
+    }
+}
